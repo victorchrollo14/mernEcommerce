657a3838a6da10bdcf7410eedae34e37308b1827
Harsh Deep <hdeep61034@gmail.com>
2023-08-05 23:23:56 +0530

    created homepage product cards component & improved the card added assets images

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,31 +1,31 @@
-import {HiOutlineShoppingCart} from 'react-icons/hi'
-function ProductCard() {
-    const data = {
-        "id": 1,
-        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        "price": 109.95,
-        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        "rating": {
-            "rate": 3.9,
-            "count": 120
-        }
-    }
-    return (
-        <div className="product-card w-[25rem] h-[37rem] rounded-lg m-5 flex flex-col">
-            <div className="product-card-image-div w-full h-[30rem] flex bg-white rounded-xl">
-                <img className="product-card-image w-9/12 m-auto" src={data.image} alt="" />
-            </div>
-            <div className="product-card-info flex mt-4 justify-between">
-                <div>
-                    <p className="product-card-title text-xl font-medi3m  my-1 text-[#686868]">{data.title}</p>
-                    <p className="product-card-price text-3xl my-1 font-semibold">${data.price}</p>
-                    </div>
-                    <div className="product-addtocart-btn h-16 w-24 flex justify-center items-center bg-[#1E1E1E] text-white rounded-3xl text-4xl relative top-1/4 ml-3"><HiOutlineShoppingCart /></div>
-            </div>
-        </div>
-    );
+import { HiOutlineShoppingCart } from 'react-icons/hi'
+function ProductCard(props) {
+	const data = {
+		"id": 1,
+		"title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+		"price": 109.95,
+		"description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+		"category": "men's clothing",
+		"image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+		"rating": {
+			"rate": 3.9,
+			"count": 120
+		}
+	}
+	return (
+		<div className="product-card w-[28rem] h-[37rem] rounded-lg m-6 flex flex-col">
+			<div className="product-card-image-div w-full h-[30rem] flex bg-white rounded-xl">
+				<img className="product-card-image w-11/12 m-auto" src={props.image} alt="" />
+			</div>
+			<div className="product-card-info flex mt-4 justify-between">
+				<div className='w-4/5'>
+					<p className="product-card-title text-xl font-light  my-1 text-[#494949]">{data.title}</p>
+					<p className="product-card-price text-3xl my-2 font-medium">${data.price}</p>
+				</div>
+				<div className="product-addtocart-btn h-full flex text-3xl"> <div className='my-auto bg-[#1E1E1E] text-white rounded-2xl p-4'><HiOutlineShoppingCart /></div></div>
+			</div>
+		</div>
+	);
 }
 
 export default ProductCard
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,28 @@
 import NavBar from "../../components/NavBar";
+import ProductCard from "../../components/ProductCard";
+import img1 from '../../assets/shirts.jpg'
+import img2 from '../../assets/outwear.jpg'
+import img3 from '../../assets/bottom.webp'
+import img4 from '../../assets/sweater.webp'
+import img5 from '../../assets/accesories.webp'
+import img6 from '../../assets/footwear.webp'
 
 const Home = () => {
-  return (
-    <>
-      <NavBar />
-      <h1 className="text-center pt-[15%] text-3xl font-semibold">Home page</h1>
-    </>);
+	return (
+		<div className=" overflow-x-hidden">
+			<NavBar />
+			<h1 className="text-center text-3xl font-semibold">Home page</h1>
+			<div className="home flex w-screen ">
+				<div className="home-product-cards w-4/5 mx-auto flex flex-wrap">
+					<ProductCard image={img1} />
+					<ProductCard image={img2} />
+					<ProductCard image={img3} />
+					<ProductCard image={img4} />
+					<ProductCard image={img5} />
+					<ProductCard image={img6} />
+				</div>
+			</div>
+		</div>);
 };
 
 export default Home
