bc656885bb1db9f86f0e6c2da372361ded34e86f
victor <chrollolucilfer1402@gmail.com>
2023-08-25 19:23:49 +0530

    add productCard.jsx, imported all images and built a responive image section for shop page

--- client/src/pages/shop/Shop.jsx
+++ client/src/pages/shop/Shop.jsx
@@ -1,14 +1,33 @@
-import NavBar from "../../components/NavBar";
+import { useEffect, useState } from "react";
 import { useProductContext } from "../../contexts/productContext";
+import NavBar from "../../components/NavBar";
+import { ProductCard } from "./productCard";
+import { Loading } from "../../components/Loading";
 
 const ShopPage = () => {
   const { products } = useProductContext();
-  console.log(products.shirts)
+  const [category, setCategory] = useState();
+
+  useEffect(() => {
+    if (products) {
+      setCategory(products.shirts);
+    }
+  }, [products]);
 
   return (
-    <div className="bg-[#DFDFDF]">
+    <div className="bg-[#DFDFDF] min-h-screen">
       <NavBar />
-      <h1>Shop Page</h1>
+      {category ? (
+        <ul className="products-list px-5 ml-0 grid grid-cols-2 sm:grid-cols-3 gap-3 lg:grid-cols-4 pt-2 ">
+          {category.map((item) => (
+            <ProductCard item={item} key={item.id} />
+          ))}
+        </ul>
+      ) : (
+        <div className="loading-screen w-screen flex justify-center">
+          <Loading />
+        </div>
+      )}
     </div>
   );
 };
---
+++ client/src/pages/shop/productCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const ProductCard = ({ item }) => {
+  const { id, src, src2, title, subtitle, price } = item;
+  const images = import.meta.glob("../ProductAssets/*/*");
+  const gallery = [];
+
+  for (const path in images) {
+    const p = new URL(path, import.meta.url).href;
+    gallery.push(p);
+  }
+  const firstImage = gallery.find((element) => element.includes(src));
+  const secondImage = gallery.find((element) => element.includes(src2));
+
+  return (
+    <li className="w-auto max-w-64 mb-5 h-fit" key={id}>
+      <figure>
+        <Link to={"/shop"} className="rounded-md">
+          <img src={firstImage} alt="" srcSet="" className="rounded-md"/>
+          {/* <img src={secondImage} alt="" /> */}
+        </Link>
+        <figcaption className="flex flex-col gap-1">
+          <span className="font-Poppins text-sm leading-none font-medium mt-2 sm:text-base sm:font-semibold " >{title}</span>
+          <span className="font-Volkhov text-xs text-[#3D4F63] sm:text-sm">{subtitle}</span>
+          <span className="font-Volkhov text-xs text-[#3D4F63] sm:text-sm">{price}</span>
+        </figcaption>
+      </figure>
+    </li>
+  );
+};
