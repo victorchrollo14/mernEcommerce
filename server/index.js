fc8f6dd854b424be74ee6d1d36b4171b1feee4d6
victor <chrollolucilfer1402@gmail.com>
2023-09-18 23:11:48 +0530

    added userSchema

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+// app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
+const runServer = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server started on ${PORT}`);
+    });
+  } catch (err) {
+    console.log(`error: ${err}`);
+  }
+};
+
+runServer();
---
+++ server/src/models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+  fullname: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  roles: {
+    type: String,
+    require: false,
+  },
+  profile: {
+    avatar: {
+      data: Buffer,
+      contentType: String,
+    },
+    shippingAddress: {
+      address: String,
+      street: String,
+      city: String,
+      state: String,
+      country: String,
+      zipcode: Number,
+    },
+  },
+  favourites: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+});
+
+const User = model("User", UserSchema);
+
+export default User;
